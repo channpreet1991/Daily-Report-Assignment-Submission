38eb1dd684bc57a600d61939f78f0634926e4831
channpreet1991 <65122644+channpreet1991@users.noreply.github.com>
2022-03-08 20:04:21 -0400

    2nd commit

---
+++ Daily Report Assignment.cs
@@ -0,0 +1,13 @@
+print("Academy of learning Career College")
+print("Student Daily report")
+
+course=input('What course are you in?:')
+
+page=int(input('what page number?'))
+
+help=input('Do you need help with anything: please answer "true or false"')
+
+experiences=input('were there anything positive you would like to share. please give specifics')
+feedback=input('Is there any other feedback you would like to provide Please give specifics')
+hours=int(input('how many hours did you study today:'))
+print('Thank you for your answers, An instructor will respondly shortly. Have a great day')
