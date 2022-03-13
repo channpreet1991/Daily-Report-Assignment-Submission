1f1d6a50863d208152047808911af717c9a6e171
channpreet1991 <65122644+channpreet1991@users.noreply.github.com>
2022-03-13 01:17:08 -0400

    3rd commit

--- Daily Report Assignment.cs
+++ Daily Report Assignment.cs
@@ -1,13 +1,34 @@
-print("Academy of learning Career College")
-print("Student Daily report")
-
-course=input('What course are you in?:')
-
-page=int(input('what page number?'))
-
-help=input('Do you need help with anything: please answer "true or false"')
-
-experiences=input('were there anything positive you would like to share. please give specifics')
-feedback=input('Is there any other feedback you would like to provide Please give specifics')
-hours=int(input('how many hours did you study today:'))
-print('Thank you for your answers, An instructor will respondly shortly. Have a great day')
+using System;
+class DailyReport {
+  static void Main() {
+      
+    Console.WriteLine("\nAcademy of Learning Career College\n");
+    Console.WriteLine("Student Daily Report\n");
+    
+    Console.WriteLine("What course are you in?");
+    string course = Console.ReadLine();
+    Console.WriteLine("\n");
+    
+    Console.WriteLine("What page number?");
+    int pageNo = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("\n");
+    
+    Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+    bool needHelp = Convert.ToBoolean(Console.ReadLine());
+    Console.WriteLine("\n");
+    
+    Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+    string posXp = Console.ReadLine();
+    Console.WriteLine("\n");
+    
+    Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+    string feedback = Console.ReadLine();
+    Console.WriteLine("\n");
+    
+    Console.WriteLine("How many hours did you study today?");
+    double studyHrs = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("\n");
+    
+    Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
+  }
+}
